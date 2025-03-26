6a15e07cd374f794e7842f073b265853f0704af5
Dheerajaldak <aldakdheeraj@gmail.com>
2025-03-26 16:30:15 +0530

    baseurl

--- LMS_FRONTEND/src/Helpers/axiosinstance.js
+++ LMS_FRONTEND/src/Helpers/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://learning-management-system-one-pi.vercel.app/";
 
 const axiosInstance = axios.create();
 
